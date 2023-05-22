02b44ef3d7d7ef13b25ea214859881196e3d3465
Bartosz Sporek <bartoszspor@gmail.com>
2023-05-22 14:47:07 +0200

    Changed CSS indicatiors, optimized code, db saving changed to csv file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
-import sqlite3
+import csv
+import os
 from sys import argv
 import babel.dates
 import datetime
@@ -8,90 +9,83 @@ import datetime
 BASE_OLX = "https://www.olx.pl"
 MAIN_OLX = f"{BASE_OLX}/d/nieruchomosci/mieszkania/wynajem/warszawa/?search%5Bprivate_business%5D=private&search%5Bfilter_float_price:from%5D=2300&search%5Bfilter_float_price:to%5D=2700&search%5Bfilter_enum_furniture%5D%5B0%5D=yes&search%5Bfilter_float_m:from%5D=35&search%5Bfilter_enum_rooms%5D%5B0%5D=two"
 PAGES_OLX = f"{MAIN_OLX}&page="
-MONTHS_OLX = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia']
-
-db = sqlite3.connect('data.db')
-cursor = db.cursor()
-
-if len(argv) > 1 and argv[1] == 'setup':
-    cursor.execute('''CREATE TABLE offers (date TEXT, name TEXT UNIQUE, city TEXT, size REAL, price REAL, link TEXT UNIQUE);''')
-    quit()
+MONTHS_OLX = {
+    'stycznia': '01',
+    'lutego': '02',
+    'marca': '03',
+    'kwietnia': '04',
+    'maja': '05',
+    'czerwca': '06',
+    'lipca': '07',
+    'sierpnia': '08',
+    'września': '09',
+    'października': '10',
+    'listopada': '11',
+    'grudnia': '12'
+}
 
 page = get(MAIN_OLX)
 bs = BeautifulSoup(page.content, 'html.parser')
-lastPage = bs.select('ul.pagination-list li:nth-of-type(5) > a:nth-of-type(1)')[0]['href']
-lp = lastPage.rsplit('=', 1)[-1]
-count = 0
-pageNum = 0
+lastPage = bs.select('ul.pagination-list li:nth-of-type(5) > a')[0].text
 numOfOffersAdded = 0
 
-for i in range(int(lp)):
+existing_rows = set()
+
+# Read existing rows from the CSV file
+if os.path.exists('flats.csv'):
+    with open('flats.csv', 'r', encoding="utf-8", newline='') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for row in reader:
+            existing_rows.add(tuple(row))
+
+with open('flats.csv','a', encoding="utf-8", newline='') as f1: 
+    writer=csv.writer(f1, delimiter='\t')
     
-    pageNum += 1
+    # Skip writing the header if the file is not empty
+    if os.stat('flats.csv').st_size == 0:
+        writer.writerow(['Data', 'Tytuł', 'Lokalizacja', 'Rozmiar', 'Cena', 'Link'])
 
-    print(f"Gathering {str(pageNum)}/{str(lp)}")
+    for page_num in range(1, int(lastPage)+1):
 
-    count+=1
-    url = PAGES_OLX + str(count)
-    rq = get(url)
-    bs = BeautifulSoup(rq.content, 'html.parser')
+        print(f"Gathering {page_num}/{lastPage}")
 
-    for offer in bs.find_all(attrs={"data-cy": "l-card"}):
-        footer = offer.find(attrs={"data-testid": "location-date"})
-        footerString = offer.find(attrs={"data-testid": "location-date"}).get_text()
-        location = footerString.split(' - ')[0]
-        dateList = (footerString.split(' - ')[1]).split()[-3:]
-        dateLong = ' '.join(dateList)
-        now = datetime.datetime.now()
-        day = dateLong.split(' ')[0]
-        month = dateLong.split(' ')[1]
-        year = dateLong.split(' ')[2]
+        url = PAGES_OLX + str(page_num)
+        rq = get(url)
+        bs = BeautifulSoup(rq.content, 'html.parser')
 
-        if month == MONTHS_OLX[0]:
-            month = "01"
-        elif month == MONTHS_OLX[1]:
-            month = "02"
-        elif month == MONTHS_OLX[2]:
-            month = "03"
-        elif month == MONTHS_OLX[3]:
-            month = "04"
-        elif month == MONTHS_OLX[4]:
-            month = "05"
-        elif month == MONTHS_OLX[5]:
-            month = "06"
-        elif month == MONTHS_OLX[6]:
-            month = "07"
-        elif month == MONTHS_OLX[7]:
-            month = "08"
-        elif month == MONTHS_OLX[8]:
-            month = "09"
-        elif month == MONTHS_OLX[9]:
-            month = "10"
-        elif month == MONTHS_OLX[10]:
-            month = "11"
-        elif month == MONTHS_OLX[11]:
-            month = "12"
+        for offer in bs.find_all(attrs={"data-cy": "l-card"}):
+            footer = offer.find(attrs={"data-testid": "location-date"})
+            footerString = offer.find(attrs={"data-testid": "location-date"}).get_text()
+            try:
+                location = (footerString.split(' - ')[0]).split(', ')[1]
+            except IndexError:
+                location = (footerString.split(' - ')[0])
+            dateList = (footerString.split(' - ')[1]).split()[-3:]
+            dateLong = ' '.join(dateList)
+            now = datetime.datetime.now()
+            day, month, year = dateLong.split(' ')
 
-        if "Dzisiaj" in dateLong:
-            dateLong = dateLong.replace(dateLong, babel.dates.format_date(now, 'yyyy/MM/dd', locale='pl_PL'))
-        else:
-            dateLong = f'{year}/{month}/{day}'
+            if month in MONTHS_OLX:
+                month = MONTHS_OLX[month]
 
-        title = offer.find('h6').get_text().strip()
-        price = offer.find(attrs={"data-testid": "ad-price"}).get_text().strip()
-        size = footer.find_next('p').get_text()
-        linkEnding = offer.find('a')['href']
-        link = BASE_OLX + linkEnding
-        if "otodom" in link:
-            link = linkEnding
+            if "Dzisiaj" in dateLong:
+                dateLong = dateLong.replace(dateLong, babel.dates.format_date(now, 'yyyy/MM/dd', locale='pl_PL'))
+            else:
+                dateLong = f'{year}/{month}/{day}'
 
-        try:
-            cursor.execute('INSERT INTO offers VALUES (?, ?, ?, ?, ?, ?)', (dateLong, title, location, size, price, link))
-            numOfOffersAdded+=1
-        except sqlite3.IntegrityError:
-            continue
+            title = offer.find('h6').get_text().strip()
+            price = offer.find(attrs={"data-testid": "ad-price"}).next_element.strip()
+            size = footer.find_next('span').get_text()
+            linkEnding = offer.find('a')['href']
+            link = BASE_OLX + linkEnding
+            if "otodom" in link:
+                link = linkEnding
 
-        db.commit()
+            row_data = [dateLong, title, location, size, price, link]
 
-db.close()
+            if tuple(row_data) not in existing_rows:
+                writer.writerow(row_data)
+                numOfOffersAdded += 1
+                existing_rows.add(tuple(row_data))
+
 print(f"{numOfOffersAdded} of the new offers were added.")
